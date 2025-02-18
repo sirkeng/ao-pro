886e4be2a383697ee7c9a22ad4e7487d8dcc239c
Natthaprug <natthaprug.b@bam.co.th>
2025-02-18 17:08:33 +0700

    feat(navigation): enhance Sidebar with dynamic menu selection and add new SVG icons

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,6 @@
 // app/(main)/layout.tsx
 "use client";
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useSelector } from "react-redux";
 import { RootState } from "@/store";
@@ -10,6 +10,12 @@ import Topbar from "@/components/Navigation/Topbar";
 export default function MainLayout({ children }: { children: ReactNode }) {
   const role = useSelector((state: RootState) => state.user.role);
   const router = useRouter();
+
+  const [selectmenu , setSelectmenu] = useState('home');
+
+  const handleSelectMenu = (menu: string) => {
+    setSelectmenu(menu);
+  }
 
   // เช็ค role ก่อนเข้าแต่ละ path ได้ตามต้องการ
   useEffect(() => {
@@ -21,7 +27,7 @@ export default function MainLayout({ children }: { children: ReactNode }) {
 
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
+      <Sidebar funtion={handleSelectMenu} select={selectmenu}/>
       <div className="flex-1 flex flex-col">
         <Topbar />
         <main className="p-4 bg-gray-50">{children}</main>
--- components/Navigation/Sidebar.tsx
+++ components/Navigation/Sidebar.tsx
@@ -5,30 +5,62 @@ import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 import { UserRole } from "@/store/features/userSlice";
 
-export default function Sidebar() {
+interface SidebarProps {
+  funtion?: any;
+  select?: string;
+}
+
+export default function Sidebar({ funtion, select }: SidebarProps) {
+
   const role = useSelector((state: RootState) => state.user.role);
 
   return (
-    <div className="w-60 bg-gray-200 p-4">
-      <div className="mb-4 font-bold">Sidebar</div>
+    <div className="w-60 bg-darkBlue">
+      <div className="w-full border-b-2 border-white py-5 flex items-center justify-center select-none">
+        <img src="/image/Logo.svg" alt="logo" className="w-12 h-12" />
+      </div>
 
-      {/* Always show Home */}
-      <Link href="/home">Home</Link>
-      <hr className="my-2" />
+      <div className="flex flex-col items-center justify-center pt-5 p-2 gap-5">
+        <Link href="/home" onClick={() => funtion('home')}
+          className={`flex flex-row items-center justify-start gap-2
+        py-2 px-8 rounded-xl text-white w-[90%]
+        duration-300 ease-in-out transition-all transform  hover:scale-105  cursor-pointer
+        ${select === 'home' ? 'bg-gradient-to-r from-[#49D8FF] to-[#5C8DBC]' : 'bg-transparent'}
+        `}
+        >
+          <img src="/image/hamberger.svg" alt="hamberger" className="cursor-pointer"></img>
+          <label className="cursor-pointer">Home</label>
+        </Link>
+        <Link href="/Overview" onClick={() => funtion('Overview')}
+          className={`flex flex-row items-center justify-start gap-2
+            py-2 px-8 rounded-xl text-white w-[90%]
+            duration-300 ease-in-out transition-all transform  hover:scale-105  cursor-pointer
+            ${select === 'Overview' ? 'bg-gradient-to-r from-[#49D8FF] to-[#5C8DBC]' : 'bg-transparent'}
+            `}>
+          <img src="/image/Overview.svg" alt="Overview" className="cursor-pointer"></img>
+          <label className="cursor-pointer">Overview</label>
+        </Link>
+        <Link href="/Details" onClick={() => funtion('Details')}
+          className={`flex flex-row items-center justify-start gap-2
+            py-2 px-8 rounded-xl text-white w-[90%]
+            duration-300 ease-in-out transition-all transform  hover:scale-105  cursor-pointer
+            ${select === 'Details' ? 'bg-gradient-to-r from-[#49D8FF] to-[#5C8DBC]' : 'bg-transparent'}
+            `}>
+          <img src="/image/Details.svg" alt="Details" className="cursor-pointer"></img>
+          <label className="cursor-pointer">Details</label>
+        </Link>
+        <Link href="/Logout" onClick={() => funtion('Logout')}
+          className={`flex flex-row items-center justify-start gap-2
+            py-2 px-8 rounded-xl text-white w-[90%]
+            duration-300 ease-in-out transition-all transform  hover:scale-105  cursor-pointer
+            ${select === 'Logout' ? 'bg-gradient-to-r from-[#49D8FF] to-[#5C8DBC]' : 'bg-transparent'}
+            `}>
+          <img src="/image/Logout.svg" alt="Logout" className="cursor-pointer"></img>
+          <label className="cursor-pointer">Log out</label>
+        </Link>
+      </div>
 
-      {/* AO Pro Navigator */}
-      {allowAONavigator(role) && (
-        <div className="mb-2">
-          <Link href="/ao-pro-navigator">AO Pro Navigator</Link>
-        </div>
-      )}
 
-      {/* Segmentation */}
-      {allowSegmentation(role) && (
-        <div className="mb-2">
-          <Link href="/segmentation-allocation">Segmentation</Link>
-        </div>
-      )}
     </div>
   );
 }
